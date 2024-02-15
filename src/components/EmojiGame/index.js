4d6b42d4fb6e0540dc41e4d4c87df52b7ec86291
prashanth <reddyprashanth512@gmail.com>
2024-02-15 17:45:12 +0000

    solution

--- src/components/EmojiGame/index.js
+++ src/components/EmojiGame/index.js
@@ -19,20 +19,37 @@ import EmojiCard from '../EmojiCard'
 import './index.css'
 
 class EmojiGame extends Component {
+  state = {clickedEmojisList: [], score: 0}
+
   onClickEmoji = id => {
     console.log(id)
     const {emojisList} = this.props
-    const shuffledList = this.shuffledEmojisList(emojisList)
-    console.log(shuffledList)
+    const {clickedEmojisList} = this.state
+    const isEmojiPresent = clickedEmojisList.includes(id)
+    const clickedEmojiLength = clickedEmojisList.length
+    if (isEmojiPresent) {
+      this.finishGameAndSetTopScore(clickedEmojiLength)
+    } else {
+      if (emojisList.length - 1 === clickedEmojiLength) {
+        this.finishGameAndSetTopScore(emojisList.length)
+      }
+
+      this.setState(prevState => ({
+        clickedEmojisList: [...prevState.clickedEmojisList, id],
+        score: prevState.score + 1,
+      }))
+    }
     return emojisList.sort(() => Math.random() - 0.5)
   }
 
   render() {
     const {emojisList} = this.props
+    const {score} = this.state
+
     return (
       <>
         <div className="emoji-container">
-          <NavBar />
+          <NavBar score={score} />
           <ul className="emoji-list-container">
             {emojisList.map(eachEmoji => (
               <EmojiCard
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,20 +1,23 @@
 // Write your code here.
 import './index.css'
 
-const NavBar = () => (
-  <div className="nav-container">
-    <div className="image-container">
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
-        alt="emoji logo"
-        className="logo"
-      />
-      <h1 className="logo-name">Emoji Game</h1>
-    </div>
-    <div className="score-container">
-      <h1 className="score">Score : 0</h1>
-      <h1 className="score-top">Top Score : 0</h1>
-    </div>
-  </div>
-)
+const NavBar = props => {
+  const {score} = props
+  return (
+    <nav className="nav-container">
+      <div className="image-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
+          alt="emoji logo"
+          className="logo"
+        />
+        <h1 className="logo-name">Emoji Game</h1>
+      </div>
+      <div className="score-container">
+        <p className="score">Score:{score}</p>
+        <p className="score-top">Top Score: 0</p>
+      </div>
+    </nav>
+  )
+}
 export default NavBar
